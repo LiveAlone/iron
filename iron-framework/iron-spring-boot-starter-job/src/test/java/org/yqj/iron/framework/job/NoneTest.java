075ef87ad4a15d6efb7cee0d38bcf88fd33a9e8b
qijunyao <qijunyao@shein.com>
2025-04-02 15:31:49 +0800

    append job

---
+++ iron-framework/iron-spring-boot-starter-job/src/test/java/org/yqj/iron/framework/job/NoneTest.java
@@ -0,0 +1,14 @@
+package org.yqj.iron.framework.job;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Description:
+ *
+ * @author yaoqijun
+ * @date 2023/6/6
+ * Email: yaoqijunmail@foxmail.com
+ */
+@Slf4j
+public class NoneTest {
+}
